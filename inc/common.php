f0197d3bd126ba25caec0f1576b1e248e0cdbfd4
Mitrij <mbox4email@gmail.com>
2024-07-28 17:11:01 +0300

    Update common.php

--- inc/common.php
+++ inc/common.php
@@ -8,9 +8,12 @@ defined( 'ABSPATH' ) || exit;
 
 function efe_add_elementor_thirdparty_cookies_condition($conditionsManager)
 {
-	require_once(EL_FUNC_EXT_BASE . '/inc/thirdparty-cookies-elementor-condition.php');
-	$args[] = new Wordpress_Adapter();
-	$condition = new \Thirdparty_Cookies_Accepted_Condition($args);
-    $conditionsManager->register_condition_instance( $condition );
+	if(function_exists('gdpr_cookie_is_accepted'))
+	{
+		require_once(EL_FUNC_EXT_BASE . '/inc/thirdparty-cookies-elementor-condition.php');
+		$args[] = new Wordpress_Adapter();
+		$condition = new \Thirdparty_Cookies_Accepted_Condition($args);
+		$conditionsManager->register_condition_instance( $condition );
+	}
 }
 add_action('elementor/display_conditions/register', 'efe_add_elementor_thirdparty_cookies_condition', 10, 1);
